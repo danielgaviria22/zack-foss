77340906eb689ab7343374ecb4a27dde7a84b24e
Juan Miguel Gomez <juanmiguelgomez8@gmail.com>
2020-03-12 11:44:25 -0500

    Minor fixes

--- src/core/utils/css-class.js
+++ src/core/utils/css-class.js
@@ -1,11 +1,11 @@
 import { toPairs, filter, propOr, join, pipe, map, head, compose } from "ramda"
 import { extract } from 'core/utils/functions'
 
-const validate = compose( extract , propOr(false,1))
+const validate = compose( extract , propOr(false,"1"))
 
 /**
  * @description receives an object of boolean values or predicates and returns the concatenation of keys that resolve to true
- * @typedef {{() => boolean}} Predicate
+ * @typedef {{(): boolean}} Predicate
  * @typedef {{ [key: string] : Predicate | boolean }} ClassConfig
  * @param {ClassConfig} classConfig
  * @example getClassName({ button: true, blue: () => false, red: () => 42 === 42 }) // returns "button red"
